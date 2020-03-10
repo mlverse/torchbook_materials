c018640583a930ee628591b5c333e1806e2a9d58
Sigrid Keydana <keydana@gmx.de>
2020-03-10 19:03:27 +0100

    intermediate save

--- scripts/bird_species.py
+++ scripts/bird_species.py
@@ -14,13 +14,18 @@ import copy
 data_transforms = {
     'train': transforms.Compose([
         transforms.RandomResizedCrop(224),
-        #transforms.ColorJitter(),
-        #transforms.RandomHorizontalFlip(),
-        #transforms.RandomRotation(30),
+        transforms.ColorJitter(),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'valid': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'test': transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
@@ -33,15 +38,15 @@ data_dir = 'data/bird_species'
 
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
                                           data_transforms[x])
-                  for x in ['train', 'valid']}
+                  for x in ['train', 'valid', 'test']}
 image_datasets
 
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
-                                             shuffle=True, num_workers=4)
-              for x in ['train', 'valid']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=16,
+                                             shuffle=True, num_workers=8)
+              for x in ['train', 'valid', 'test']}
 dataloaders
 
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'valid']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'valid', 'test']}
 dataset_sizes
 
 class_names = image_datasets['train'].classes
@@ -111,7 +116,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
             # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
+            if phase == 'valid' and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model_wts = copy.deepcopy(model.state_dict())
         print()
@@ -124,27 +129,6 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     return model
 
 
-def visualize_model(model, num_images=6):
-    was_training = model.training
-    model.eval()
-    images_so_far = 0
-    fig = plt.figure()
-    with torch.no_grad():
-        for i, (inputs, labels) in enumerate(dataloaders['valid']):
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
-            for j in range(inputs.size()[0]):
-                images_so_far += 1
-                ax = plt.subplot(num_images//2, 2, images_so_far)
-                ax.axis('off')
-                ax.set_title('predicted: {}'.format(class_names[preds[j]]))
-                imshow(inputs.cpu().data[j])
-                if images_so_far == num_images:
-                    model.train(mode=was_training)
-                    return
-        model.train(mode=was_training)
 
 model = models.resnet18(pretrained=True)
 model
@@ -152,21 +136,37 @@ model
 for param in model.parameters():
     param.requires_grad = False
 
-num_ftrs = model.fc.in_features
+num_features = model.fc.in_features
 
-model.fc = nn.Linear(num_ftrs,  len(class_names))
+model.fc = nn.Linear(num_features, len(class_names))
 
 model = model.to(device)
 
 criterion = nn.CrossEntropyLoss()
 
-# Observe that all parameters are being optimized
-optimizer_ft = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
 # Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
-model_ft = train_model(model, criterion, optimizer_ft, exp_lr_scheduler,
+model = train_model(model, criterion, optimizer, exp_lr_scheduler,
                        num_epochs=1)
 
-visualize_model(model)
+def eval_model(model):
+    was_training = model.training
+    model.eval()
+    with torch.no_grad():
+        total = 0
+        corrects = 0
+        for i, (inputs, labels) in enumerate(dataloaders['test']):
+            total += labels.size(0)
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            corrects += torch.sum(preds == labels.data)
+        model.train(mode=was_training)
+    print('test Acc: {:4f}'.format(corrects/total))
+
+eval_model(model)
+
