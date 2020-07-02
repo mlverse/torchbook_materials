dbebbf1cb890d929653d8524029f55e94927b72b
skeydan <keydana@gmx.de>
2020-07-02 08:42:27 +0200

    update for latest enhancements

---
+++ scripts/tabular.R
@@ -0,0 +1,21 @@
+# https://yashuseth.blog/2018/07/22/pytorch-neural-network-for-tabular-data-with-categorical-embeddings/
+
+# https://towardsdatascience.com/deep-learning-for-tabular-data-using-pytorch-1807f2858320
+
+# http://francescopochetti.com/pytorch-for-tabular-data-predicting-nyc-taxi-fares/
+
+
+# City name
+# Scope-1 GHG emissions [tCO2 or tCO2-eq]
+# Scope-1 source dataset
+# Year of emission
+# Country
+# Region
+#
+# Gases included (CDP)
+#
+# Population (CDP)
+# Population (carbonn)
+# Urban population (PKU)
+
+library(torch)
--- scripts/tabular.py
+++
@@ -1,5 +0,0 @@
-#https://yashuseth.blog/2018/07/22/pytorch-neural-network-for-tabular-data-with-categorical-embeddings/
-
-# https://towardsdatascience.com/deep-learning-for-tabular-data-using-pytorch-1807f2858320
-
-# http://francescopochetti.com/pytorch-for-tabular-data-predicting-nyc-taxi-fares/
