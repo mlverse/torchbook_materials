7e683f962c639fffa811a52a4cb363ee4b92dd4f
skeydan <keydana@gmx.de>
2020-07-10 14:32:59 +0200

    use new vision_make_grid

--- scripts/vae.R
+++ scripts/vae.R
@@ -131,40 +131,6 @@ num_epochs <- 3
 
 img_list <- vector(mode = "list", length = num_epochs * trunc(dl$.iter()$.length()/50))
 
-normalize <- function(x) {
-    min = x$min()$item()
-    max = x$max()$item()
-    x$clamp_(min = min, max = max)
-    x$add_(-min)$div_(max - min + 1e-5)
-    x
-}
-
-# 4D mini-batch Tensor of shape (B x C x H x W)
-make_grid <- function(tensor, num_rows = 8, padding = 2, pad_value = 0) {
-    nmaps <- tensor$size(0)
-    xmaps <- min(num_rows, nmaps)
-    ymaps <- ceiling(nmaps/xmaps)
-    height <- floor(tensor$size(2) + padding)
-    width <- floor(tensor$size(3) + padding)
-    num_channels <- tensor$size(1)
-    grid <- tensor$new_full(c(num_channels, height * ymaps + padding, width * xmaps + padding), pad_value)
-    k <- 0
-    for (y in 0:(ymaps - 1)) {
-        for (x in 0:(xmaps - 1)) {
-            if (k >= nmaps) break
-            grid$narrow(
-                dim = 1,
-                start = torch_tensor(y * height + padding, dtype = torch_int64())$sum(dim = 0),
-                length = height - padding)$narrow(
-                    dim = 2,
-                    start = torch_tensor(x * width + padding, dtype = torch_int64())$sum(dim = 0),
-                    length = width - padding)$copy_(tensor[k + 1, , , ])
-            k <- k + 1
-        }
-    }
-    grid
-}
-
 img_num <- 0
 for (epoch in 1:num_epochs) {
 
@@ -187,7 +153,7 @@ for (epoch in 1:num_epochs) {
                 "\n")
             with_no_grad({
                 generated <- model$sample(64, device)
-                grid <- make_grid(normalize(generated))
+                grid <- vision_make_grid(generated)
                 img_list[[img_num]] <- as_array(grid$to(device = "cpu"))
             })
 
