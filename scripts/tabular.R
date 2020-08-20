d82e31c8112b33bd676121023bc0195b5d5e2d22
skeydan <keydana@gmx.de>
2020-08-20 11:00:32 +0200

    switch to mushroom dataset

--- scripts/tabular.R
+++ scripts/tabular.R
@@ -1,59 +1,53 @@
-# https://doi.org/10.1594/PANGAEA.884141
+# https://archive.ics.uci.edu/ml/datasets/Mushroom
 
-# The dataset builds upon data from CDP (187 cities, few in developing countries), the Bonn Center for Local Climate Action and Reporting (73 cities, mainly in developing countries), and data collected by Peking University (83 cities in China).
-
-# Scope-1 emissions cover GHGs emitted in the city territory, including emissions from grid-supplied energy produced within cities
 
 library(torch)
 library(tidyverse)
 
-co2_data <- read_tsv("../torchbook/data/co2/D_FINAL.tsv") %>%
-  select(
-    # Five data records related to Scope-1 emissions and three data records related to geo-descriptive attributes were set as common columns in Dfinal to avoid redundancy.
-    `Scope-1 source dataset`,
 
-    `Scope-1 GHG emissions [tCO2 or tCO2-eq]`,
-    `Year of emission`,
+mushroom_data <- read_csv(
+  "data/agaricus-lepiota.data",
+  col_names = c(
+    "poisonous",
+    "cap-shape",
+    "cap-surface",
+    "cap-color",
+    "bruises",
+    "odor",
+    "gill-attachment",
+    "gill-spacing",
+    "gill-size",
+    "gill-color",
+    "stalk-shape",
+    "stalk-root",
+    "stalk-surface-above-ring",
+    "stalk-surface-below-ring",
+    "stalk-color-above-ring",
+    "stalk-color-below-ring",
+    "veil-type",
+    "veil-color",
+    "ring-type",
+    "ring-number",
+    "spore-print-color",
+    "population",
+    "habitat"
+  ),
+  col_types = rep("c", 23) %>% paste(collapse = "")
+)
 
-    `Country`,
-    `Region`,
+train_indices <- sample(1:nrow(mushroom_data), size = floor(0.8 * nrow(mushroom_data)))
+valid_indices <- setdiff(1:nrow(mushroom_data), train_indices)
 
-    `Population (others)`,
-    `Household size (others) [people/household]`,
-    `City area (others) [km2]`,
+mushroom_dataset <- dataset(
+  name = "mushroom_dataset",
 
-    `HDD 15.5C (clim) [degrees C × days]`,
-    `CDD 23C (clim) [degrees C × days]`,
-
-    `Diesel price 2014 (others) [USD/liter]`,
-    `Gasoline price 2014 (others) [USD/liter]`
-  ) %>%
-  rename (
-    source = `Scope-1 source dataset`,
-    tco2 = `Scope-1 GHG emissions [tCO2 or tCO2-eq]`,
-    year = `Year of emission`,
-    country = `Country`,
-    region = `Region`,
-    population = `Population (others)`,
-    household_size = `Household size (others) [people/household]`,
-    city_area = `City area (others) [km2]`,
-    hdd = `HDD 15.5C (clim) [degrees C × days]`,
-    cdd = `CDD 23C (clim) [degrees C × days]`,
-    diesel_price = `Diesel price 2014 (others) [USD/liter]`,
-    gasoline_price = `Gasoline price 2014 (others) [USD/liter]`
-  ) %>%
-  filter(!is.na(tco2))
-
-co2_dataset <- dataset(
-  name = "co2_dataset",
-
-  initialize = function() {
-    self$data <- self$prepare_co2_data(co2_data)
+  initialize = function(indices) {
+    self$data <- self$prepare_mushroom_data(mushroom_data[indices, ])
   },
 
   .getitem = function(index) {
     x <- self$data[index, 2:-1]
-    y <- self$data[index, 1]
+    y <- self$data[index, 1] - 1
 
     list(x, y)
   },
@@ -62,109 +56,108 @@ co2_dataset <- dataset(
     self$data$size()[[1]]
   },
 
-  prepare_co2_data = function(co2) {
-    input <- cbind(
-      self$min_max_scale(co2$tco2),
-      as.integer(as.factor(co2$country)) - 1,
-      as.integer(as.factor(co2$region)) - 1,
-      self$min_max_scale(co2$year),
-      self$min_max_scale(self$fill_na(co2$population)),
-      self$min_max_scale(self$fill_na(co2$household_size)),
-      self$min_max_scale(self$fill_na(co2$city_area)),
-      self$min_max_scale(co2$hdd),
-      self$min_max_scale(co2$cdd),
-      self$min_max_scale(co2$diesel_price),
-      self$min_max_scale(co2$gasoline_price)
-    )
+  prepare_mushroom_data = function(input) {
+    input <- input %>%
+      mutate_all(compose(as.integer, as.factor)) %>%
+      as.matrix()
 
-    torch_tensor(input)
-  },
-
-  min_max_scale = function(x) {
-    min = min(x)
-    max = max(x)
-    (x - min) / (max - min + 1e-5)
-  },
-
-  fill_na = function(x) {
-    fill_value <- mean(na.omit(x))
-    x[is.na(x)] <- fill_value
-    x
+    torch_tensor(input)$to(torch_long())
   }
 )
 
-ds <- co2_dataset()
-ds$.length()
-ds$.getitem(1)
+train_ds <- mushroom_dataset(train_indices)
+train_ds$.length()
+train_ds$.getitem(1)
 
-dl <- ds %>% dataloader(batch_size = 8)
-dl$.length()
+train_dl <- train_ds %>% dataloader(batch_size = 256, shuffle = TRUE)
+train_dl$.length()
 
-iter <- dl$.iter()
+iter <- train_dl$.iter()
 b <- iter$.next()
 b
 
+valid_ds <- mushroom_dataset(valid_indices)
+valid_ds$.length()
+
+valid_dl <- valid_ds %>% dataloader(batch_size = 256, shuffle = FALSE)
+valid_dl$.length()
+
 net <- nn_module(
-  "CO2Net",
-  initialize = function(num_countries,
-                        num_regions,
-                        country_emb_dim,
-                        region_emb_dim,
-                        num_numeric,
+  "mushroom_net",
+
+  initialize = function(cardinalities,
                         fc1_dim,
                         fc2_dim) {
-    self$country_embedding <-
-      nn_embedding(num_countries, country_emb_dim)
-    self$region_embedding <-
-      nn_embedding(num_regions, region_emb_dim)
-    self$fc1 <- nn_linear(num_numeric, fc1_dim)
-    self$fc2 <- nn_linear(fc1_dim + country_emb_dim + region_emb_dim, fc2_dim)
+    self$embeddings <- vector(mode = "list", length = length(cardinalities))
+    for (i in 1:length(cardinalities)) {
+      self$embeddings[[i]] <- nn_embedding(cardinalities[i], ceiling(cardinalities[i]/2))
+    }
+    self$fc1 <- nn_linear(sum(map(cardinalities, function(x) ceiling(x/2)) %>% unlist()), fc1_dim)
+    self$fc2 <-
+      nn_linear(fc1_dim, fc2_dim)
     self$output <- nn_linear(fc2_dim, 1)
   },
+
   forward = function(x) {
-    x_country_emb <- self$country_embedding(x[ , 1, drop = FALSE]) %>%
-      nnf_relu()
-    x_region_emb <- self$region_embedding(x[ , 2, drop = FALSE]) %>%
-      nnf_relu()
-    x_numeric <- self$fc1(x[ , 3:-1]) %>%
-      nnf_relu()
-    self$fc2(torch_cat(x_country_emb, x_region_emb, x_numeric)) %>%
+    xs <- vector(mode = "list", length = length(self$embeddings))
+    for (i in 1:length(self$embeddings)) {
+      xs[[i]] <- self$embeddings[[i]](x[ , i])
+    }
+    self$fc1(torch_cat(xs, dim = 2)) %>%
       nnf_relu() %>%
-      self$output()
+      self$fc2() %>%
+      self$output() %>%
+      nnf_sigmoid()
   }
 )
 
-num_countries <- as.factor(co2_data$country) %>% nlevels()
-num_regions <- as.factor(co2_data$region) %>% nlevels()
-country_emb_dim <- 7
-region_emb_dim <- 4
-num_numeric <- 8
+cardinalities <- map(mushroom_data[ , 2:ncol(mushroom_data)], compose(nlevels, as.factor)) %>%
+  unlist() %>%
+  unname()
+
 fc1_dim <- 64
 fc2_dim <- 64
 
-model <- net(num_countries,
-             num_regions,
-             country_emb_dim,
-             region_emb_dim,
-             num_numeric,
-             fc1_dim,
-             fc2_dim)
+model <- net(
+  cardinalities,
+  fc1_dim,
+  fc2_dim
+)
 
-optimizer <- optim_adam(model$parameters)
+optimizer <- optim_sgd(model$parameters, lr = 0.1)
 
-for (epoch in 1:10) {
+for (epoch in 1:20) {
 
-  l <- c()
+  model$train()
+  train_losses <- c()
 
-  for (b in enumerate(dl)) {
+  for (b in enumerate(train_dl)) {
     optimizer$zero_grad()
     output <- model(b[[1]])
-    loss <- nnf_nll_loss(output, b[[2]])
+    loss <- nnf_binary_cross_entropy(output, b[[2]]$to(torch_float()))
     loss$backward()
     optimizer$step()
-    l <- c(l, loss$item())
+    train_losses <- c(train_losses, loss$item())
   }
 
-  cat(sprintf("Loss at epoch %d: %3f\n", epoch, mean(l)))
+  model$eval()
+  valid_losses <- c()
+
+  for (b in enumerate(valid_dl)) {
+    output <- model(b[[1]])
+    loss <- nnf_binary_cross_entropy(output, b[[2]]$to(torch_float()))
+    valid_losses <- c(valid_losses, loss$item())
+  }
+
+  cat(sprintf("Loss at epoch %d: training: %3f, validation: %3f\n", epoch, mean(train_losses), mean(valid_losses)))
 }
 
+model$eval()
+test_dl <- valid_ds %>% dataloader(batch_size = valid_ds$.length(), shuffle = FALSE)
+iter <- test_dl$.iter()
+b <- iter$.next()b
+preds <- as_array(model(b[[1]]))
+preds <- ifelse(preds > 0.5, 1, 0)
+comp_df <- data.frame(preds = preds, y = b[[2]] %>% as_array())
+sum(comp_df$preds == comp_df$y)
+sum(comp_df$y == 1)
