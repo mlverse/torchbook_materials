4508b1ec2ebf910521a46d3dd5912e7c1fc2a581
skeydan <keydana@gmx.de>
2021-02-24 07:29:36 +0100

    remove nao

--- scripts/vic_elec.R
+++ scripts/vic_elec.R
@@ -64,26 +64,46 @@ vic_elec_2014 %>% features(Demand, feat_spectral) #[0, 1]
 
 n_timesteps <- 7 * 24 * 2
 
-elec_train <- vic_elec_2014$Demand %>% as.matrix()
+vic_elec_2012 <- vic_elec %>%
+  filter(year(Date) == 2012) %>%
+  select(-c(Date, Holiday))
+
+elec_train <- vic_elec_2012$Demand %>% as.matrix()
+
+vic_elec_2013 <- vic_elec %>%
+  filter(year(Date) == 2013) %>%
+  select(-c(Date, Holiday))
+
+elec_valid <- vic_elec_2013$Demand %>% as.matrix()
+
+vic_elec_jan_2014 <- vic_elec %>%
+  filter(yearmonth(Date) == yearmonth("2014-01")) %>%
+  select(-c(Date, Holiday))
+
+elec_test <- vic_elec_jan_2014$Demand %>% as.matrix()
+
+
+train_mean <- mean(elec_train)
+train_sd <- sd(elec_train)
 
 elec_dataset <- dataset(
   name = "elec_dataset",
   
   initialize = function(demand, n_timesteps) {
-    self$demand <-demand
+    self$demand <- (demand - train_mean) / train_sd
     self$n_timesteps <- n_timesteps
     
   },
   
   .getitem = function(i) {
     
-    x <- torch_tensor(self$demand[i:(n_timesteps + i - 1)])$unsqueeze(2)
-    y <- torch_tensor(self$demand[n_timesteps + i])
+    x <- torch_tensor(self$demand[i:(self$n_timesteps + i - 1)])$unsqueeze(2)
+    y <- torch_tensor(self$demand[self$n_timesteps + i])
     list(x = x, y = y)
   },
   
   .length = function() {
-    length(self$demand) - n_timesteps
+    length(self$demand) - self$n_timesteps
   }
   
 )
@@ -102,17 +122,14 @@ length(train_dl)
 # b <- iter %>% dataloader_next()
 # b
 
-vic_elec_2015 <- vic_elec %>%
-  filter(year(Date) == 2014) %>%
-  select(-c(Date, Holiday))
-
-elec_valid <- vic_elec_2015$Demand %>% as.matrix()
-
 valid_ds <- elec_dataset(elec_valid, n_timesteps)
 #length(valid_ds)
 valid_dl <- valid_ds %>% dataloader(batch_size = batch_size)
 length(valid_dl)
 
+test_ds <- elec_dataset(elec_test, n_timesteps)
+test_dl <- test_ds %>% dataloader(batch_size = 1)
+length(test_dl)
 
 # model -------------------------------------------------------------------
 
@@ -168,7 +185,7 @@ net <- net$to(device = device)
 
 optimizer <- optim_adam(net$parameters, lr = 0.001)
 
-num_epochs <- 300
+num_epochs <- 10
 
 train_batch <- function(b) {
   
@@ -188,7 +205,7 @@ train_batch <- function(b) {
   
   loss$backward()
   optimizer$step()
-
+  
   loss$item()
   
 }
@@ -217,7 +234,7 @@ for (epoch in 1:num_epochs) {
   })
   
   if (epoch %% 100 == 0) torch_save(net, paste0("model_", epoch, ".pt"))
-  cat(sprintf("\nEpoch %d, training: loss: %3.3f \n", epoch, mean(train_loss)))
+  cat(sprintf("\nEpoch %d, training: loss: %3.5f \n", epoch, mean(train_loss)))
   
   net$eval()
   valid_loss <- c()
@@ -227,7 +244,7 @@ for (epoch in 1:num_epochs) {
     valid_loss <- c(valid_loss, loss)
   })
   
-  cat(sprintf("\nEpoch %d, validation: loss: %3.3f \n", epoch, mean(valid_loss)))
+  cat(sprintf("\nEpoch %d, validation: loss: %3.5f \n", epoch, mean(valid_loss)))
 }
 
 
@@ -237,27 +254,60 @@ net$eval()
 
 preds <- rep(NA, n_timesteps)
 
-train_dl <- train_ds %>% dataloader(batch_size = batch_size, shuffle = FALSE)
-
-#dl <- train_dl
-dl <- valid_dl
-
-#ds <- nao_train
-ds <- nao_valid
-
-#cutoff <- "1987-12"
-cutoff <- "2015-12"
+dl <-test_dl
+ds <- elec_test
 
 coro::loop(for (b in dl) {
   output <- net(b$x$to(device = device))
   preds <- c(preds, output %>% as.numeric())
 })
 
-preds_ts <- ds %>%
-  add_column(preds) %>%
-  pivot_longer(-x) %>%
+preds_ts <- vic_elec_jan_2014 %>% select(-Temperature) %>%
+  add_column(preds * train_sd + train_mean) %>%
+  pivot_longer(-Time) %>%
   update_tsibble(key = name)
 
 preds_ts %>%
-  filter(x > yearmonth(cutoff)) %>%
-  autoplot()
+  autoplot() +
+  scale_colour_manual(values = c("cyan", "violet")) +
+  theme_minimal()
+
+# predict in loop ---------------------------------------------------------
+
+n_forecast <- n_timesteps
+
+test_preds <- vector(mode = "list", length = length(test_dl))
+
+i <- 1
+
+coro::loop(for (b in test_dl) {
+  
+  print(i)
+  input <- b$x
+  output <- net(input$to(device = device))
+  preds <- as.numeric(output)
+  
+  for(j in 2:n_forecast) {
+    input <- torch_cat(list(input[ , 2:length(input), ], output$view(c(1, 1, 1))), dim = 2)
+    output <- net(input$to(device = device))
+    preds <- c(preds, as.numeric(output))
+  }
+  
+  test_preds[[i]] <- preds
+  i <<- i + 1
+  
+})
+
+test_pred <- test_preds[[1]]
+test_pred <- c(rep(NA, n_timesteps), test_pred, rep(NA, nrow(vic_elec_jan_2014) - n_timesteps - n_forecast))
+
+preds_ts <- vic_elec_jan_2014 %>% select(-Temperature) %>%
+  add_column(test_pred * train_sd + train_mean) %>%
+  pivot_longer(-Time) %>%
+  update_tsibble(key = name)
+
+preds_ts %>%
+  autoplot() +
+  scale_colour_manual(values = c("cyan", "violet")) +
+  theme_minimal()
+
