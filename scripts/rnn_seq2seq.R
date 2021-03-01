ac7af616179e5967e282e78a12cfa4e9ef823548
skeydan <keydana@gmx.de>
2021-03-01 10:31:54 +0100

    add seq2seq

---
+++ scripts/rnn_seq2seq.R
@@ -0,0 +1,299 @@
+library(torch)
+library(tidyverse)
+library(tsibble)
+library(tsibbledata)
+library(lubridate)
+library(fable)
+
+
+# datasets -----------------------------------------------------------------
+
+n_timesteps <- 7 * 24 * 2
+n_forecast <- n_timesteps
+
+vic_elec_get_year <- function(year, month = NULL) {
+  vic_elec %>%
+    filter(year(Date) == year, month(Date) == if (is.null(month)) month(Date) else month) %>%
+    as_tibble() %>%
+    select(Demand)
+}
+
+elec_train <- vic_elec_get_year(2012) %>% as.matrix()
+elec_valid <- vic_elec_get_year(2013) %>% as.matrix()
+elec_test <- vic_elec_get_year(2014, 1) %>% as.matrix()
+
+train_mean <- mean(elec_train)
+train_sd <- sd(elec_train)
+
+elec_dataset <- dataset(
+  name = "elec_dataset",
+  
+  initialize = function(demand, n_timesteps) {
+    self$demand <- (demand - train_mean) / train_sd
+    self$n_timesteps <- n_timesteps
+    
+  },
+  
+  .getitem = function(i) {
+    
+    x <- torch_tensor(self$demand[i:(self$n_timesteps + i - 1)])$unsqueeze(2)
+    y <- torch_tensor(self$demand[(i + 1):(self$n_timesteps + i)])
+    list(x = x, y = y)
+  },
+  
+  .length = function() {
+    length(self$demand) - 2 * self$n_timesteps + 1
+  }
+  
+)
+
+train_ds <- elec_dataset(elec_train, n_timesteps)
+length(train_ds)
+
+batch_size <- 32
+train_dl <- train_ds %>% dataloader(batch_size = batch_size, shuffle = TRUE)
+length(train_dl)
+
+valid_ds <- elec_dataset(elec_valid, n_timesteps)
+valid_dl <- valid_ds %>% dataloader(batch_size = batch_size)
+length(valid_dl)
+
+test_ds <- elec_dataset(elec_test, n_timesteps)
+test_dl <- test_ds %>% dataloader(batch_size = 1)
+length(test_dl)
+
+# model -------------------------------------------------------------------
+
+encoder_module <- nn_module(
+  
+  initialize = function(type, input_size, hidden_size, num_layers = 1, dropout = 0) {
+    
+    self$type <- type
+    
+    self$rnn <- if (self$type == "gru") {
+      nn_gru(
+        input_size = input_size,
+        hidden_size = hidden_size,
+        num_layers = num_layers,
+        dropout = dropout,
+        batch_first = TRUE
+      )
+    } else {
+      nn_lstm(
+        input_size = input_size,
+        hidden_size = hidden_size,
+        num_layers = num_layers,
+        dropout = dropout,
+        batch_first = TRUE
+      )
+    }
+    
+  },
+  
+  forward = function(x) {
+    
+    x <- self$rnn(x)
+  
+    # return last states for all layers
+    # per layer, a single tensor for GRU, a list of 2 tensors for LSTM
+    x <- x[[2]]
+    x
+    
+  }
+  
+)
+
+decoder_module <- nn_module(
+  
+  initialize = function(type, input_size, hidden_size, num_layers = 1, dropout = 0) {
+    
+    self$type <- type
+    
+    self$rnn <- if (self$type == "gru") {
+      nn_gru(
+        input_size = input_size,
+        hidden_size = hidden_size,
+        num_layers = num_layers,
+        dropout = dropout,
+        batch_first = TRUE
+      )
+    } else {
+      nn_lstm(
+        input_size = input_size,
+        hidden_size = hidden_size,
+        num_layers = num_layers,
+        dropout = dropout,
+        batch_first = TRUE
+      )
+    }
+    
+    self$linear <- nn_linear(hidden_size, 1)
+    
+  },
+  
+  forward = function(x, state) {
+    
+    # state is (1, batch_size, hidden_size)
+    # x is (batch_size, 1, 1)
+    x <- self$rnn(x, state)
+    # (batch_size, 1, hidden_size)
+    output <- x[[1]]
+    # (batch_size, hidden_size)
+    output <- output$squeeze(2)
+    output <- self$linear(output)
+    
+    next_hidden <- x[[2]]
+    
+    list(output, next_hidden)
+    
+    
+  }
+  
+)
+
+seq2seq_module <- nn_module(
+  
+  initialize = function(type, input_size, hidden_size, n_forecast, teacher_forcing_ratio, num_layers = 1, dropout = 0) {
+    
+    self$encoder <- encoder_module(type = type, input_size = input_size, hidden_size = hidden_size, num_layers, output)
+    self$decoder <- decoder_module(type = type, input_size = input_size, hidden_size = hidden_size, num_layers, output)
+    self$n_forecast <- n_forecast
+    
+  },
+  
+  forward = function(x, y, teacher_forcing_ratio) {
+    
+    outputs <- torch_zeros(dim(x)[1], self$n_forecast)$to(device = device)
+    hidden <- self$encoder(x)
+    # list of (batch_size, 1), (1, batch_size, hidden_size)
+    out <- self$decoder(x[ , n_timesteps, , drop = FALSE], hidden)
+    # (batch_size, 1)
+    pred <- out[[1]]
+    # (1, batch_size, hidden_size)
+    state <- out[[2]]
+    outputs[ , 1] <- pred$squeeze(2)
+    
+    for (t in 2:self$n_forecast) {
+      
+      teacher_forcing <- runif(1) < teacher_forcing_ratio
+      input <- if (teacher_forcing == TRUE) pred$unsqueeze(3) else y[ , t - 1]
+      out <- self$decoder(pred$unsqueeze(3), state)
+      pred <- out[[1]]
+      state <- out[[2]]
+      outputs[ , t] <- pred$squeeze(2)
+      
+    }
+    
+    outputs
+  }
+  
+)
+
+device <- torch_device(if (cuda_is_available()) "cuda" else "cpu")
+device <- "cpu"
+
+net <- seq2seq_module("gru", input_size = 1, hidden_size = 32, n_forecast = n_forecast, teacher_forcing_ratio = 1)
+net <- net$to(device = device)
+
+b <- dataloader_make_iter(train_dl) %>% dataloader_next()
+net(b$x, b$y, teacher_forcing_ratio = 1)
+
+# train -------------------------------------------------------------------
+
+optimizer <- optim_adam(net$parameters, lr = 0.001)
+
+num_epochs <- 30
+
+train_batch <- function(b, teacher_forcing_ratio) {
+  
+  optimizer$zero_grad()
+  output <- net(b$x$to(device = device), b$y$to(device = device), teacher_forcing_ratio)
+  target <- b$y$to(device = device)
+  
+  loss <- nnf_mse_loss(output, target[ , 1:(dim(output)[2])])
+  loss$backward()
+  optimizer$step()
+  
+  loss$item()
+  
+}
+
+valid_batch <- function(b, teacher_forcing_ratio = 0) {
+  
+  output <- net(b$x$to(device = device), b$y$to(device = device), teacher_forcing_ratio)
+  target <- b$y$to(device = device)
+  
+  loss <- nnf_mse_loss(output, target[ , 1:(dim(output)[2])])
+  
+  loss$item()
+  
+}
+
+for (epoch in 1:num_epochs) {
+  
+  net$train()
+  train_loss <- c()
+  
+  coro::loop(for (b in train_dl) {
+    loss <-train_batch(b, teacher_forcing_ratio = 1)
+    train_loss <- c(train_loss, loss)
+  })
+  
+  cat(sprintf("\nEpoch %d, training: loss: %3.5f \n", epoch, mean(train_loss)))
+  
+  net$eval()
+  valid_loss <- c()
+  
+  coro::loop(for (b in valid_dl) {
+    loss <- valid_batch(b)
+    valid_loss <- c(valid_loss, loss)
+  })
+  
+  cat(sprintf("\nEpoch %d, validation: loss: %3.5f \n", epoch, mean(valid_loss)))
+}
+
+torch_save(net, "model_seq2seq.pt")
+
+# predict ---------------------------------------------------------
+
+net$eval()
+
+test_preds <- vector(mode = "list", length = length(test_dl))
+
+coro::loop(for (b in test_dl) {
+  
+  input <- b$x
+  output <- net(input$to(device = device), teacher_forcing_ratio = 0)
+  preds <- as.numeric(output)
+  
+  test_preds[[i]] <- preds
+  i <<- i + 1
+  
+})
+
+test_pred1 <- test_preds[[1]]
+test_pred1 <- c(rep(NA, n_timesteps), test_pred1, rep(NA, nrow(vic_elec_jan_2014) - n_timesteps - n_forecast))
+
+test_pred2 <- test_preds[[408]]
+test_pred2 <- c(rep(NA, n_timesteps + 407), test_pred2, rep(NA, nrow(vic_elec_jan_2014) - 407 - n_timesteps - n_forecast))
+
+test_pred3 <- test_preds[[817]]
+test_pred3 <- c(rep(NA, nrow(vic_elec_jan_2014) - n_forecast), test_pred3)
+
+
+preds_ts <- vic_elec %>%
+  filter(year(Date) == 2014, month(Date) == 1) %>%
+  select(Demand) %>%
+  add_column(
+    iterative_ex_1 = test_pred * train_sd + train_mean,
+    iterative_ex_2 = test_pred2 * train_sd + train_mean,
+    iterative_ex_3 = test_pred3 * train_sd + train_mean) %>%
+  pivot_longer(-Time) %>%
+  update_tsibble(key = name)
+
+
+preds_ts %>%
+  autoplot() +
+  scale_colour_manual(values = c("#08c5d1", "#00353f", "#ffbf66", "#d46f4d")) +
+  theme_minimal()
+
